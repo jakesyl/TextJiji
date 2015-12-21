9f73afef56554521be8746d7472d6b9673c441e1
stefanstokic <stefan@stefanstokic.com>
2015-12-21 01:30:36 -0600

    Initial Bot

---
+++ README.md
@@ -0,0 +1,3 @@
+Start running server via
+
+`token=xoxb-17113663408-B4TmHaOrK19JHvAgrIWdyn node app.js`
---
+++ app.js
@@ -0,0 +1,79 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+          \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+           \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+
+
+This is a sample Slack bot built with Botkit.
+
+This bot demonstrates many of the core features of Botkit:
+
+* Connect to Slack using the real time API
+* Receive messages based on "spoken" patterns
+* Send a message with attachments
+* Send a message via direct message (instead of in a public channel)
+
+# RUN THE BOT:
+
+  Get a Bot token from Slack:
+
+    -> http://my.slack.com/services/new/bot
+
+  Run your bot from the command line:
+
+    token=<MY TOKEN> node team_bot.js
+
+# USE THE BOT:
+
+  Find your bot inside Slack to send it a direct message.
+
+  Say: "Hello"
+
+  The bot will reply "Hello!"
+
+  Say: "Attach"
+
+  The bot will send a message with a multi-field attachment.
+
+  Send: "dm"
+
+  The bot will reply with a direct message.
+
+  Make sure to invite your bot into other channels using /invite @<my bot>!
+
+# EXTEND THE BOT:
+
+  Botkit is has many features for building cool and useful bots!
+
+  Read all about it here:
+
+    -> http://howdy.ai/botkit
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var Botkit = require('botkit');
+
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+ debug: false,
+});
+
+controller.spawn({
+  token: xoxb-17113663408-B4TmHaOrK19JHvAgrIWdyn
+}).startRTM(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+
+controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
+    bot.reply(message,"Hello.");
+})
