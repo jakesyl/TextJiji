1b177619f82bf2e56cb9e3a6a350cb3ed1e38341
Henry Kaufman <hkaufman19@gmail.com>
2015-12-21 00:45:22 -0700

    AYY LMAO

--- app.js
+++ app.js
@@ -1,79 +1,34 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-          ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-          \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-           \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Send a message with attachments
-* Send a message via direct message (instead of in a public channel)
-
-# RUN THE BOT:
-
-  Get a Bot token from Slack:
-
-    -> http://my.slack.com/services/new/bot
-
-  Run your bot from the command line:
-
-    token=<MY TOKEN> node team_bot.js
-
-# USE THE BOT:
-
-  Find your bot inside Slack to send it a direct message.
-
-  Say: "Hello"
-
-  The bot will reply "Hello!"
-
-  Say: "Attach"
-
-  The bot will send a message with a multi-field attachment.
-
-  Send: "dm"
-
-  The bot will reply with a direct message.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
-
-# EXTEND THE BOT:
-
-  Botkit is has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
 var Botkit = require('botkit');
+var request = require('request');
 
 
 if (!process.env.token) {
-  console.log('Error: Specify token in environment');
-  process.exit(1);
+    console.log('Error: Specify token in environment');
+    process.exit(1);
 }
 
 var controller = Botkit.slackbot({
- debug: false,
+    debug: false,
 });
 
 controller.spawn({
-  token: process.env.token
+    token: process.env.token
 }).startRTM(function(err) {
-  if (err) {
-    throw new Error(err);
-  }
+    if (err) {
+        throw new Error(err);
+    }
 });
 
 
-controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
-    bot.reply(message,"Hello.");
-})
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, "Hello.");
+});
+
+controller.hears([':alien:'], 'direct_message,direct_mention,mention', function(bot, message) {
+    request('http://api.giphy.com/v1/gifs/search?q=ayy+lmao&api_key=dc6zaTOxFJmzC&limit=1', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body.date[0].url);
+        }
+    })
+    bot.reply(message, "Hello.");
+});
