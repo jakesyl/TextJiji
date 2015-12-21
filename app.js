8b2acce090ec19ac87c39a137d1adbe48e4b8365
Henry Kaufman <hkaufman19@gmail.com>
2015-12-21 00:48:45 -0700

    relevant json

--- app.js
+++ app.js
@@ -27,8 +27,9 @@ controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', funct
 controller.hears([':alien:'], 'direct_message,direct_mention,mention', function(bot, message) {
     request('http://api.giphy.com/v1/gifs/search?q=ayy+lmao&api_key=dc6zaTOxFJmzC&limit=1', function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body.date[0].url);
+            console.log(body.data[0].images.fixed_height.url);
+            bot.reply(message, body.data[0].images.fixed_height.url);
         }
     })
-    bot.reply(message, "Hello.");
+
 });
